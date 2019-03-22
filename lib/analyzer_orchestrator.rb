281b03b01f79bd62968872a219e12edd08d15479
Jeremy Walker <jez.walker@gmail.com>
2019-03-22 17:53:54 +0000

    Smoke test propono messages

--- lib/analyzer_orchestrator.rb
+++ lib/analyzer_orchestrator.rb
@@ -6,6 +6,7 @@ require "analyzer_orchestrator/publish_message"
 require "analyzer_orchestrator/listen_for_new_iterations"
 
 module AnalyzerOrchestrator
-  # Your code goes here...
-
+  def self.listen
+    ListenForNewIterations.()
+  end
 end
--- lib/ext/propono.rb
+++ lib/ext/propono.rb
@@ -4,14 +4,14 @@ require 'erb'
 module Propono
   def self.configure_client
     Client.new do |config|
-      creds = YAML::load(ERB.new(File.read(File.dirname(__FILE__) + "/../../config/propono.yml.erb")).result)
+      creds = YAML::load(ERB.new(File.read(File.dirname(__FILE__) + "/../../config/secrets.yml")).result)
 
       creds = creds["development"]
 
-      config.access_key = creds["access_key"]
-      config.secret_key = creds["secret_key"]
-      config.queue_region = creds["queue_region"]
-      config.queue_suffix = creds["queue_suffix"] || ""
+      config.access_key = creds["aws_access_key_id"]
+      config.secret_key = creds["aws_secret_access_key"]
+      config.queue_region = creds["aws_region"]
+      config.queue_suffix = creds["aws_queue_suffix"] || ""
       config.application_name = creds["application_name"]
       # config.logger = Rails.logger
     end
