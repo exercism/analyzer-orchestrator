012edc5b7881f92d10b73a801c31f7f422791d02
Charles Care <c.p.care@gmail.com>
2019-03-22 17:34:12 +0000

    Wire in config

--- lib/analyzer_orchestrator.rb
+++ lib/analyzer_orchestrator.rb
@@ -6,4 +6,6 @@ require "analyzer_orchestrator/publish_message"
 require "analyzer_orchestrator/listen_for_new_iterations"
 
 module AnalyzerOrchestrator
+  # Your code goes here...
+
 end
--- lib/ext/propono.rb
+++ lib/ext/propono.rb
@@ -1,19 +1,19 @@
+require 'yaml'
+require 'erb'
+
 module Propono
   def self.configure_client
     Client.new do |config|
-      creds = YAML::load(ERB.new(File.read("#{__FILE__}../../config/propono.yml.erb")).result).stringify_keys
-      creds = (creds[Rails.env]).symbolize_keys
+      creds = YAML::load(ERB.new(File.read(File.dirname(__FILE__) + "/../../config/propono.yml.erb")).result)
 
-      if creds[:use_iam_profile]
-        config.use_iam_profile = true
-      else
-        config.access_key = creds[:access_key]
-        config.secret_key = creds[:secret_key]
-      end
-      config.queue_region = creds[:queue_region]
-      config.queue_suffix = creds[:queue_suffix] || ""
-      config.application_name = creds[:application_name]
-      config.logger = Rails.logger
+      creds = creds["development"]
+
+      config.access_key = creds["access_key"]
+      config.secret_key = creds["secret_key"]
+      config.queue_region = creds["queue_region"]
+      config.queue_suffix = creds["queue_suffix"] || ""
+      config.application_name = creds["application_name"]
+      # config.logger = Rails.logger
     end
   end
 end
