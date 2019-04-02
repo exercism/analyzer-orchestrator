3a4101274cc2a818b7fef1054a65779ac803662f
Jeremy Walker <jez.walker@gmail.com>
2019-04-02 23:42:54 +0100

    Add correct release command

--- lib/orchestrator/deploy_analyzer.rb
+++ lib/orchestrator/deploy_analyzer.rb
@@ -2,10 +2,10 @@ module Orchestrator
   class DeployAnalyzer
     include Mandate
 
-    initialize_with :image_name
+    initialize_with :track_slug, :image_name
 
     def call
-      cmd = %Q{echo "Deploy analyzer #{image_name}"}
+      cmd = %Q{release_analyzer #{track_slug} #{image_name}}
       Kernel.system(cmd)
     end
   end
