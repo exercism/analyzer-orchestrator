a0f28db03c94c8d848d8a12c1c9c0458b5bffbfe
Jeremy Walker <jez.walker@gmail.com>
2019-04-08 17:04:45 +0100

    Add logging (#6)

--- lib/orchestrator/listen_for_new_iterations.rb
+++ lib/orchestrator/listen_for_new_iterations.rb
@@ -4,6 +4,9 @@ module Orchestrator
 
     def call
       client.listen(:new_iteration) do |message|
+        p "Received message"
+        p message
+
         track_slug = message[:track_slug]
         exercise_slug = message[:exercise_slug]
         iteration_id = message[:iteration_id]
