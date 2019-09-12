1bd7024507bf16619f01d43e8100e0485de50026
Jeremy Walker <jez.walker@gmail.com>
2019-09-12 13:15:13 +0100

    Tidy whitespace

--- lib/orchestrator/analyze_iteration.rb
+++ lib/orchestrator/analyze_iteration.rb
@@ -4,27 +4,27 @@ module Orchestrator
     ['csharp', 'two-fer'],
     ['csharp', 'leap'],
     ['csharp', 'gigasecond'],
-    
+
     ['go', 'two-fer'],
     ['go', 'hamming'],
     ['go', 'raindrops'],
-    
+
     ['java', 'two-fer'],
     ['java', 'hamming'],
-    
+
     ['javascript', 'two-fer'],
     ['javascript', 'resistor-color'],
     ['javascript', 'gigasecond'],
-    
+
     ['python', 'two-fer'],
-    
+
     ['ruby', 'two-fer'],
-    
+
     ['rust', 'reverse-string'],
 
     ['scala', 'two-fer'],
     ['scala', 'leap'],
-    
+
     ['typescript', 'two-fer']
   ]
 
